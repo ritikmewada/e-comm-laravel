e9cd8a03267f5ef9c03f7a9012f6faaa29a43709
Ritik Mewada <mewadaritik05042002@gmail.com>
2023-01-18 15:25:00 +0530

    login page done

---
+++ resources/views/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-comm Project</title>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+</head>
+<body>
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
+</body>
+<style>
+    .custom-login{
+        height: 500px;
+        padding-top: 100px;
+    }
+</style>
+</html>
