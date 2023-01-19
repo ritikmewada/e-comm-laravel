4c9d57d389c6f5f7c21dedcc412d008ca55e3bff
Ritik Mewada <mewadaritik05042002@gmail.com>
2023-01-19 11:23:59 +0530

    login done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">E-Comm</a>
+      <a class="navbar-brand" href="#">Brand</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -13,7 +13,7 @@
             <a class="nav-link" href="#">Orders</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Add to Cart</a>
+            <a class="nav-link" href="#">Cart(0)</a>
           </li>
         </ul>
         <form class="d-flex">
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::view("/","login");
+Route::get('/login', function (){
+    return view('login');
+});
+Route::post("/login",[UserController::class,'login']);
+Route::get("/",[ProductController::class,'index']);
